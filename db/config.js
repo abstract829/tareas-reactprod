d7dc0aadfa8033b7dec45bd3f20ff8fef2d09b6f
abstract829 <maxxii.1820@gmail.com>
2021-12-12 18:43:35 -0300

    probando

--- db/config.js
+++ db/config.js
@@ -1,11 +1,10 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-    host:'localhost',
-    user: 'postgres',
-    password: '123',
-    database: 'back-tareas',
-    port: '5432'
+    connectionString : 'postgres://gfjcqurysqkekp:6364c2649e40109b8dccb01360f45371d3611329a141f87fcf96a7c8e9c92eab@ec2-3-230-219-251.compute-1.amazonaws.com:5432/dddhdl7g1u18ii',
+    ssl:{
+        rejectUnauthorized: false
+    }
 })
 
 module.exports = {
--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require ('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
-
+app.use( express.static('public') );
 app.use(cors())
 app.use(express.json())
 
 app.use('/tareas', require('./routes/tareas'))
 app.use('/users', require('./routes/users'))
 
+app.get('*', (req,res) =>{
+    res.sendFile( path.resolve(__dirname, 'public/index.html'))
+})
 app.listen(process.env.PORT, console.log('Server iniciado en el puerto: ' + process.env.PORT))
