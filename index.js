1dc74b154554f955a25abf244476d16cae376f13
abstract829 <maxxii.1820@gmail.com>
2021-12-13 02:16:00 -0300

    refresh bug

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ require('dotenv').config()
 app.use( express.static('public') );
 app.use(cors())
 app.use(express.json())
-
 app.use('/api/tareas', require('./routes/tareas'))
 app.use('/api/users', require('./routes/users'))
 
 app.get('*', (req,res) =>{
-    res.sendFile( path.resolve(__dirname, 'public/index.html'))
+    res.sendFile( path.join(__dirname, 'public/index.html'), (err) => {
+        if(err){
+            res.status(500).send(__dirname)
+        }
+    })
 })
 app.listen(process.env.PORT, console.log('Server iniciado en el puerto: ' + process.env.PORT))
